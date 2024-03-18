2d56ebe3574527401f9affe906be81a171674664
empm <emcommunitymanager@gmail.com>
2024-03-18 19:59:35 +0100

    add: ej 1

---
+++ Ej1.java
@@ -0,0 +1,25 @@
+package U09Colecciones;
+
+/**
+ * Ej1
+ * 
+ * ArrayList<String> listaPaises = new ArrayList();
+ * listaPaises.add("España"); //Ocupa la posición 0
+ * listaPaises.add("Francia"); //Ocupa la posición 1
+ * listaPaises.add("Portugal"); //Ocupa la posición 2
+ * 
+ * ArrayList<String> listaPaises = new ArrayList();
+ * listaPaises.add("España");
+ * listaPaises.add("Francia");
+ * listaPaises.add("Portugal");
+ * //El orden hasta ahora es: España, Francia, Portugal
+ * listaPaises.add(1, "Italia");
+ * //El orden ahora es: España, Italia, Francia, Portugal
+ * 
+ * System.out.println(listaPaises.get(3));
+ * //Siguiendo el ejemplo anterior, mostraría: Portugal
+ */
+public class Ej1 {
+
+
+}
