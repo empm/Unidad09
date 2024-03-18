267a20ed4cc6ce20198bb544d8c9b630f26fa821
empm <emcommunitymanager@gmail.com>
2024-03-18 20:41:27 +0100

    feat: fin ej1

--- Ej1.java
+++ Ej1.java
@@ -1,25 +1,31 @@
 package U09Colecciones;
 
+import java.util.ArrayList;
+
 /**
  * Ej1
- * 
- * ArrayList<String> listaPaises = new ArrayList();
- * listaPaises.add("España"); //Ocupa la posición 0
- * listaPaises.add("Francia"); //Ocupa la posición 1
- * listaPaises.add("Portugal"); //Ocupa la posición 2
- * 
- * ArrayList<String> listaPaises = new ArrayList();
- * listaPaises.add("España");
- * listaPaises.add("Francia");
- * listaPaises.add("Portugal");
- * //El orden hasta ahora es: España, Francia, Portugal
- * listaPaises.add(1, "Italia");
- * //El orden ahora es: España, Italia, Francia, Portugal
- * 
- * System.out.println(listaPaises.get(3));
- * //Siguiendo el ejemplo anterior, mostraría: Portugal
+ *
+ * 1.- Desenvolupar el ArrayList listaPaises vist en els exemples del tema
  */
 public class Ej1 {
+    public static void main(String[] args) {
+        ArrayList<String> listaPaises = new ArrayList<>();
 
+        listaPaises.add("España");
+        listaPaises.add("Portugal");
+        listaPaises.add("Francia");
+        System.out.println(listaPaises.get(2));
+        listaPaises.add(2, "Italia");
+        listaPaises.addFirst("Alemania");
+        listaPaises.remove(4);
+
+
+        System.out.println("Total paises: " + listaPaises.size());
+
+        for (String lista : listaPaises){
+            System.out.println(lista);
+        }
+
+    }
 
 }
