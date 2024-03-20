b6496ea2644f3359c1c4d13f8f9e560b80a47e59
empm <emcommunitymanager@gmail.com>
2024-03-20 22:14:04 +0100

    feat: desarrollando enunciado

--- EjerciciosColecciones/Ej3/Ej3.java
+++ EjerciciosColecciones/Ej3/Ej3.java
@@ -1,5 +1,30 @@
 package U09.EjerciciosColecciones.Ej3;
 
+/*
+ * Entendiendo enunciado
+ */
+/*
+ * Autopista peaje / 3 ventanas
+ * 1. Efectivo
+ * 2. Exacto
+ * 3. Tarjeta
+ * ?? esta en mayus porque puede ser enum?
+ * 
+ * Se actua como cola: el primero en entrar, es el primero en salir.
+ * 
+ * los datos que se almacenan de los coches en las colas:
+ * 1. matricula
+ * 2. modelo
+ * 3. color
+ * 
+ * La app tiene 3 metodos
+ * 1. añadir coche al final de la cola
+ * 2. eliminar coche del principio de la cola
+ * 3. salir de la app
+ * ?? se me ocurre un bucle while
+ * 
+ */
 public class Ej3 {
     
+
 }
